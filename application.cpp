c4a4640fba00bd98e681b9bd1ff63f2470cf8b6f
NapierDanel <40511533@live.napier.ac.uk>
2021-05-08 13:03:11 +0200

    added index.html

---
+++ application.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+
+    String getJobGoal =
+}
+
+class JobSuche
+{
+
+    class job
+    {
+        string zeit;
+        string[] learning_skills;
+
+        Map<String, String[]>();
+
+        string[] bewerbung;
+        string[] conditions;
+
+    }
+
+    Zeit : 1,
+        5 Monate;
+
+    Job
+    {
+
+    goal:
+        "Werkstudent",
+            learning_scills : "Ausbildung", "programmierung_lernen", "server_einrichten", "server_verwalten", "",
+
+                              Bewerbungen : ""
+
+                                            conditions : ""
+
+                                                         goal : "BachelorArbeit",
+                                                                learning_scills : "Ausbildung",
+                                                                                  "programmierung_lernen",
+                                                                                  "server_einrichten",
+                                                                                  "server_verwalten",
+                                                                                  "",
+
+                                                                                  Bewerbungen : ""
+    }
+}
